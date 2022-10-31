4ccae7c08f56798201c4ae9f43d78109f5fe8774
Joao Victor Piloni <jvpiloni@gmail.com>
2022-10-31 03:28:02 -0300

    Adição do Geolocation API

--- geolocation/src/app/app.component.ts
+++ geolocation/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {GeolocationService} from '@ng-web-apis/geolocation';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'geolocation';
+
+  constructor(private readonly geolocation$: GeolocationService) {}
+  
+  getPosition() {
+    this.geolocation$.subscribe(position => 
+     console.log(position));
+}
+
+ngOnInit() {
+  this.getPosition();
+}
+
 }
